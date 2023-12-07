db46cd29c16fcf65662ca27109612339a44374eb
Surax98 <tedesk95@gmail.com>
2023-12-07 16:21:27 +0100

    add offloading setup

---
+++ AF20/interlink/condor-setup/Bari.sh
@@ -0,0 +1,7 @@
+export CONDOR_CONFIG=$PWD/condor_config
+export _condor_COLLECTOR_HOST=ce-01.recas.ba.infn.it:9619
+export _condor_SCHEDD_HOST=ce-01.recas.ba.infn.it
+export _condor_TOOL_DEBUG=D_FULLDEBUG,D_SECURITY
+export _condor_SEC_DEFAULT_AUTHENTICATION_METHODS=GSI
+#export X509_USER_CERT=$PWD/proxy_daniele_new_new_new
+export X509_USER_PROXY=$PWD/proxy/proxy
